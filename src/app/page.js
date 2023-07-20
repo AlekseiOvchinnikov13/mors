df747522d69a5868bb7ffda37511a979882c4d64
Albina Zakirova <ZakirovaAlbina@yandex.com>
2023-07-20 16:42:33 +0300

    add About

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import FirstScreen from '@/components/FirstScreen';
+import About from '@/components/About';
 
 export default function Home() {
   return (
     <>
       <FirstScreen/>
+      <About/>
     </>
   );
 }
